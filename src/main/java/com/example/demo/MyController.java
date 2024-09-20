2b1e52810f5fb4f2f7270279125fb0140dd7edd8
eugenewongyj <eugenewongyj@gmail.com>
2024-09-20 11:30:30 +0800

    added zipwith code

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -11,10 +11,16 @@ public class MyController {
 
     @GetMapping("/demo")
     public Mono<String> greetingMessage() {
-        return computeMesssage();
+        return computeMesssage()
+                .zipWith(getNameFromDB())
+                .map(value -> value.getT1() + value.getT2());
     }
 
     private Mono<String> computeMesssage() {
         return Mono.just("Hello ").delayElement(Duration.ofSeconds(5));
+    }
+
+    private Mono<String> getNameFromDB() {
+        return Mono.just("Eugene").delayElement(Duration.ofSeconds(5));
     }
 }
