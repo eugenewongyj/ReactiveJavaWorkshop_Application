dc4e7fde5211da45cfaf204419f11d3be443a472
eugenewongyj <eugenewongyj@gmail.com>
2024-09-20 11:24:22 +0800

    Edited code to return mono

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 public class MyController {
 
     @GetMapping("/demo")
-    public String greetingMessage() {
-        return "Hello World";
+    public Mono<String> greetingMessage() {
+        return computeMesssage();
+    }
+
+    private Mono<String> computeMesssage() {
+        return Mono.just("Hello ").delayElement(Duration.ofSeconds(5));
     }
 }
